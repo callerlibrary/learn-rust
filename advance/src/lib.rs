f3de4f62985414d491b90406dccdd7a23ebfea1d
dingchen <chen.ding@senscape.com.cn>
2023-11-20 17:22:40 +0800

    ✨ feat<lib>: 闭包理解

--- advance/src/lib.rs
+++ advance/src/lib.rs
@@ -50,6 +50,8 @@ pub mod lifetime {
 
 // 闭包
 pub mod closure {
+    use std::thread;
+    use std::time::Duration;
     pub fn closure() {
         let mut x: i32 = 999i32;
         let mut c = || x += 1;
@@ -57,5 +59,39 @@ pub mod closure {
         c();
         c();
         println!("{}", x);
+    }
+
+    // 开始健身，好累，我得发出声音：muuuu...
+    fn muuuuu(intensity: u32) -> u32 {
+        println!("muuuu.....");
+        thread::sleep(Duration::from_secs(2));
+        intensity
+    }
+
+    fn workout(intensity: u32, random_number: u32) {
+        if intensity < 25 {
+            println!("今天活力满满，先做 {} 个俯卧撑!", muuuuu(intensity));
+            println!(
+                "旁边有妹子在看，俯卧撑太low，再来 {} 组卧推!",
+                muuuuu(intensity)
+            );
+        } else if random_number == 3 {
+            println!("昨天练过度了，今天还是休息下吧！");
+        } else {
+            println!(
+                "昨天练过度了，今天干干有氧，跑步 {} 分钟!",
+                muuuuu(intensity)
+            );
+        }
+    }
+
+    pub fn run() {
+        // 强度
+        let intensity = 10;
+        // 随机值用来决定某个选择
+        let random_number = 7;
+
+        // 开始健身
+        workout(intensity, random_number);
     }
 }
