0df2ffb35748bbd474245ff686df70e4404749bf
dingchen <chen.ding@senscape.com.cn>
2023-11-21 15:00:58 +0800

    ✨ feat<app>: 去除默认代码，预添加JSON功能

--- learn-tauri/src/App.vue
+++ learn-tauri/src/App.vue
@@ -1,52 +1,33 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
-</script>
-
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-
-    <Greet />
+  <div>
+    <el-form :model="form" label-width="120px">
+      <el-form-item label="JSON">
+        <el-input type="textarea" autosize v-model="form.json" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Submit</el-button>
+        <el-button>Cancel</el-button>
+      </el-form-item>
+    </el-form>
+    <el-form label-width="120px">
+      <el-form-item label="RESULT">
+        <el-input type="textarea" autosize disabled v-model="result" />
+      </el-form-item>
+    </el-form>
+    
   </div>
 </template>
 
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+<script setup lang="ts">
+import { ref } from 'vue'
+const form = ref({ json: '' })
+// 结果
+const result = ref('')
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
+const onSubmit = () => {
+  console.log('submit!', form.value)
+  // TODO 生成序列化后的结果
 }
-</style>
+</script>
+
+<style scoped></style>
