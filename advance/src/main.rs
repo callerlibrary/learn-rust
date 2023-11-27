f2183333b21abbfca7bdfeb9dee3b2f3e1aa060e
dingchen <chen.ding@senscape.com.cn>
2023-11-27 15:30:26 +0800

    ✨ feat<advance>: async/.await了解

--- advance/src/main.rs
+++ advance/src/main.rs
@@ -1,7 +1,11 @@
+mod async_ts_mod;
 mod macro_ts_mod;
 
 use advance::*;
+use async_ts_mod::*;
 use macro_ts_mod::*;
+
+use futures::executor::*;
 fn main() {
     // lifetime::life_cycle();
     // closure::closure();
@@ -18,4 +22,10 @@ fn main() {
     // 引入vec_ts宏
     let v = vec_ts![1, 2, 3];
     println!("{:?}", v);
+
+    async_ts::ts();
+    let future = async_ts::do_something();
+    block_on(future);
+
+    block_on(async_ts::async_main());
 }
