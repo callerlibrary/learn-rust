91895eb1dd7565bb702b9739e400547e83ced246
dingchen <chen.ding@senscape.com.cn>
2023-11-27 13:33:01 +0800

    ✨ feat<advance>: 错误处理

--- advance/src/main.rs
+++ advance/src/main.rs
@@ -3,5 +3,9 @@ fn main() {
     // lifetime::life_cycle();
     // closure::closure();
     // thread_ts::thread_run();
-    thread_ts::thread_move();
+    // thread_ts::thread_move();
+    err_ts::os_and_ts();
+    err_ts::or_else_ts();
+    err_ts::ang_then_ts();
+    err_ts::filter_ts();
 }
